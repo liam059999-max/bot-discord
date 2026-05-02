4c6c090991056b9650b5cfb9795d64288070225d
liam059999-max <liam.059999@gmail.com>
2026-05-03 00:46:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -247,12 +247,11 @@ async def on_member_join(member: discord.Member):
         inviter_text = inviter.mention if inviter else "l'url de Nebulix RP"
 
         await channel.send(
-            f"👋 Bienvenue à {member.mention}\n"
-            f"📩 Il a rejoint avec {inviter_text}
-"
-            f"👑 Il a désormais **{total_uses} invitation(s)**\n"
-            f"⭐ Nous sommes désormais **{member.guild.member_count}** sur le discord !"
-        )
+    f"👋 Bienvenue à {member.mention}\n"
+    f"📩 Il a rejoint avec {inviter_text}\n"
+    f"👑 Il a désormais **{total_uses} invitation(s)**\n"
+    f"⭐ Nous sommes désormais **{member.guild.member_count}** sur le discord !"
+)
 
 
 @client.event
